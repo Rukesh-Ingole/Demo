9e70fd02d960b4ef9b0dad23d798b25082461d1a
Rukesh-Ingole <67459565+Rukesh-Ingole@users.noreply.github.com>
2020-10-10 23:52:16 +0530

    Update Demo.java

--- GitDemoNew/src/Git/Demo.java
+++ GitDemoNew/src/Git/Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Hello Git World");
 	System.out.println("Git New Change");
 	System.out.println("Changes");
+	System.out.println("Hello Git Server")
 }
 }
